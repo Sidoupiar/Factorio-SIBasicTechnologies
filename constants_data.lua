48c46a61342cc2baab3c54ab24e4d4e2721dcc87
Sidoupiar <sidoupiar@qq.com>
2021-05-19 14:16:32 +0800

    基础结构

--- constants_data.lua
+++ constants_data.lua
@@ -3,4 +3,16 @@ return
 	base = "BasicTechnologies" ,
 	autoLoad = true ,
 	autoName = true ,
+	
+	
+	
+	group =
+	{
+		logistics = "logistics" ,
+		machine   = "production" ,
+		item      = "intermediate-products" ,
+		weapon    = "combat" ,
+		fluid     = "fluids" ,
+		signal    = "signals"
+	}
 }
--- data.lua
+++ data.lua
@@ -4,10 +4,12 @@ load()
 -- ----------- 初始化 -----------------------------------------------------------------------------
 -- ------------------------------------------------------------------------------------------------
 
-SIGen
-.Init( SIBT )
-.NewGroup( "agriculture" )
+SIBT.group.agriculture = SIGen.Init( SIBT ).NewGroup( "agriculture" ).GetCurrentEntityName()
 
-needlist( "zprototype" , "1_carbon" )
+needlist( "zprototype/1_material" , "1_resource" , "2_paint" , "3_tool" , "4_food" )
+needlist( "zprototype/2_cycle" , "1_carbon" , "2_science" , "3_elepanel" )
+needlist( "zprototype/3_level" , "1_machine" , "2_generator" , "3_minedrill" , "4_container" , "5_module" , "6_equipment" )
+
+needlist( "zprototype/9_special" , "1_transbelt" , "2_pipe" , "3_railway" )
 
 SIGen.Finish()
