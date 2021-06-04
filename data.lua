6fc72cb1feee5da66c145def25871676a4f2569a
Sidoupiar <sidoupiar@qq.com>
2021-06-04 15:42:37 +0800

    优化代码

--- data.lua
+++ data.lua
@@ -8,6 +8,7 @@ SIGen
 .Init( SIBT )
 .NewGroup( SIBT.group.agriculture )
 
+needlist( "zprototype" , "1_common" )
 needlist( "zprototype/1_material" , "1_resource" , "2_paint" , "3_tool" , "4_food" )
 needlist( "zprototype/2_cycle" , "1_carbon" , "2_science" , "3_elepanel" )
 needlist( "zprototype/3_level" , "1_machine" , "2_generator" , "3_minedrill" , "4_container" , "5_module" , "6_equipment" , "7_weapon" )
